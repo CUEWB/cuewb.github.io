f261c0600d5630448fa7adda93c609faf33e94d1
Gwen Pfetsch <gwen.pfetsch@gmail.com>
2014-09-23 15:46:49 -0400

    adds select class to nav element when clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     $(".main").onepage_scroll();
+    selectNav();
 });
+
+function selectNav() {
+	$('a[href*=#]').click(function () {
+		$("#nav-menu li.selected").removeClass("selected");
+		var hash = $(this).attr("href");
+    	$('#nav-menu li a[href="'+ hash+ '"]').parent().addClass("selected");
+    })
+}
