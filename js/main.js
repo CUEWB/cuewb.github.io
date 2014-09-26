2949dd53dc8357580dc04cc6651a7e199147eaeb
Gwen Pfetsch <gwen.pfetsch@gmail.com>
2014-09-26 18:40:44 -0400

    saving changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-    $(".main").onepage_scroll();
-    selectNav();
+	scrollToAnchor();
+	selectOnScroll();
+	selectNav();
 });
 
 function selectNav() {
@@ -9,4 +10,33 @@ function selectNav() {
 		var hash = $(this).attr("href");
     	$('#nav-menu li a[href="'+ hash+ '"]').parent().addClass("selected");
     })
+}
+
+function selectOnScroll() {
+	$("#nav-menu li a").each(function() {
+        var id = $(this).attr("href");
+        /*var array = {"#1":"#home", "#2":"#our-programs", "#3":"#events", "#4":"#contacts"};*/
+        $(id).waypoint(function(direction) {
+            if (direction === "down") {
+                $('#nav-menu li.selected').removeClass('selected');
+                $('#nav-menu li a[href="' + id +'"]').parent()
+                                                     .addClass('selected');
+            }
+            else if (direction === "up" && id !== "#home") {
+                $('#nav-menu li.selected').removeClass('selected');
+                $('#nav-menu li a[href="' + id +'"]').parent().prev()
+                                                     .addClass('selected');
+            }
+        });
+    });
+}
+
+function scrollToAnchor(){
+	$('a[href^="#"]').each(function() {
+	    var id = $(this).attr('href');
+	    $(this).click(function(e) {
+	        e.preventDefault();
+            $('html,body').animate({scrollTop: $(id).offset().top+1},'slow');
+        });
+    });
 }
