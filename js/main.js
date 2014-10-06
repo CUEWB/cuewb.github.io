4a6754814fad43f574a82272c4ed0afe688d2324
Gwen Pfetsch <gwen.pfetsch@gmail.com>
2014-10-05 20:21:12 -0400

    getting rid of one page scroll

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-$(document).ready(function() {
-	scrollToAnchor();
-	selectOnScroll();
-	selectNav();
-});
+// $(document).ready(function() {
+// 	scrollToAnchor();
+// 	selectOnScroll();
+// 	selectNav();
+// });
 
-function selectNav() {
-	$('a[href*=#]').click(function () {
-		$("#nav-menu li.selected").removeClass("selected");
-		var hash = $(this).attr("href");
-    	$('#nav-menu li a[href="'+ hash+ '"]').parent().addClass("selected");
-    })
-}
+// function selectNav() {
+// 	$('a[href*=#]').click(function () {
+// 		$("#nav-menu li.selected").removeClass("selected");
+// 		var hash = $(this).attr("href");
+//     	$('#nav-menu li a[href="'+ hash+ '"]').parent().addClass("selected");
+//     })
+// }
 
-function selectOnScroll() {
-	$("#nav-menu li a").each(function() {
-        var id = $(this).attr("href");
-        /*var array = {"#1":"#home", "#2":"#our-programs", "#3":"#events", "#4":"#contacts"};*/
-        $(id).waypoint(function(direction) {
-            if (direction === "down") {
-                $('#nav-menu li.selected').removeClass('selected');
-                $('#nav-menu li a[href="' + id +'"]').parent()
-                                                     .addClass('selected');
-            }
-            else if (direction === "up" && id !== "#home") {
-                $('#nav-menu li.selected').removeClass('selected');
-                $('#nav-menu li a[href="' + id +'"]').parent().prev()
-                                                     .addClass('selected');
-            }
-        });
-    });
-}
+// function selectOnScroll() {
+// 	$("#nav-menu li a").each(function() {
+//         var id = $(this).attr("href");
+//         /*var array = {"#1":"#home", "#2":"#our-programs", "#3":"#events", "#4":"#contacts"};*/
+//         $(id).waypoint(function(direction) {
+//             if (direction === "down") {
+//                 $('#nav-menu li.selected').removeClass('selected');
+//                 $('#nav-menu li a[href="' + id +'"]').parent()
+//                                                      .addClass('selected');
+//             }
+//             else if (direction === "up" && id !== "#home") {
+//                 $('#nav-menu li.selected').removeClass('selected');
+//                 $('#nav-menu li a[href="' + id +'"]').parent().prev()
+//                                                      .addClass('selected');
+//             }
+//         });
+//     });
+// }
 
-function scrollToAnchor(){
-	$('a[href^="#"]').each(function() {
-	    var id = $(this).attr('href');
-	    $(this).click(function(e) {
-	        e.preventDefault();
-            $('html,body').animate({scrollTop: $(id).offset().top+1},'slow');
-        });
-    });
-}
+// function scrollToAnchor(){
+// 	$('a[href^="#"]').each(function() {
+// 	    var id = $(this).attr('href');
+// 	    $(this).click(function(e) {
+// 	        e.preventDefault();
+//             $('html,body').animate({scrollTop: $(id).offset().top+1},'slow');
+//         });
+//     });
+// }
